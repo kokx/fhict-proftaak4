2d8e31b107f000cfa4682542da8ce7f1b015da92
Alex Ruijs <ginaf60@hotmail.com>
2010-05-18 13:21:40 +0200

    cometaar uitgewerkt

--- laptop/Form1.Designer.cs
+++ laptop/Form1.Designer.cs
@@ -29,21 +29,27 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbRobot2X = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbRobot2Y = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.tbRobot1X = new System.Windows.Forms.TextBox();
+            this.tbPersoonY = new System.Windows.Forms.TextBox();
             this.lbCoordinatenRobot = new System.Windows.Forms.Label();
             this.lbCoordinaatPersoon = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tbPersoonX = new System.Windows.Forms.TextBox();
             this.btZendCoordinaten = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbRobot1Y = new System.Windows.Forms.TextBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.btKomTerug = new System.Windows.Forms.Button();
             this.btVolgendeStap = new System.Windows.Forms.Button();
+            this.btStatus = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -51,28 +57,74 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.tbRobot2X);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.tbRobot2Y);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.textBox4);
-            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.tbRobot1X);
+            this.groupBox1.Controls.Add(this.tbPersoonY);
             this.groupBox1.Controls.Add(this.lbCoordinatenRobot);
             this.groupBox1.Controls.Add(this.lbCoordinaatPersoon);
-            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.tbPersoonX);
             this.groupBox1.Controls.Add(this.btZendCoordinaten);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.tbRobot1Y);
             this.groupBox1.Location = new System.Drawing.Point(12, 144);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(146, 242);
+            this.groupBox1.Size = new System.Drawing.Size(214, 242);
             this.groupBox1.TabIndex = 14;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "coordinaten";
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(108, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(12, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "y";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(108, 134);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(12, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "x";
+            // 
+            // tbRobot2X
+            // 
+            this.tbRobot2X.Location = new System.Drawing.Point(126, 131);
+            this.tbRobot2X.Name = "tbRobot2X";
+            this.tbRobot2X.Size = new System.Drawing.Size(75, 20);
+            this.tbRobot2X.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(111, 105);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(96, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "coordinaten robot2";
+            // 
+            // tbRobot2Y
+            // 
+            this.tbRobot2Y.Location = new System.Drawing.Point(126, 157);
+            this.tbRobot2Y.Name = "tbRobot2Y";
+            this.tbRobot2Y.Size = new System.Drawing.Size(75, 20);
+            this.tbRobot2Y.TabIndex = 12;
+            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 160);
+            this.label3.Location = new System.Drawing.Point(9, 160);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(12, 13);
             this.label3.TabIndex = 10;
@@ -81,7 +133,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 134);
+            this.label4.Location = new System.Drawing.Point(9, 134);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(12, 13);
             this.label4.TabIndex = 11;
@@ -90,7 +142,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 80);
+            this.label1.Location = new System.Drawing.Point(47, 80);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(12, 13);
             this.label1.TabIndex = 8;
@@ -99,30 +151,30 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Location = new System.Drawing.Point(47, 54);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(12, 13);
             this.label2.TabIndex = 9;
             this.label2.Text = "x";
             // 
-            // textBox4
+            // tbRobot1X
             // 
-            this.textBox4.Location = new System.Drawing.Point(30, 131);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(75, 20);
-            this.textBox4.TabIndex = 9;
+            this.tbRobot1X.Location = new System.Drawing.Point(27, 131);
+            this.tbRobot1X.Name = "tbRobot1X";
+            this.tbRobot1X.Size = new System.Drawing.Size(75, 20);
+            this.tbRobot1X.TabIndex = 9;
             // 
-            // textBox3
+            // tbPersoonY
             // 
-            this.textBox3.Location = new System.Drawing.Point(30, 77);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(75, 20);
-            this.textBox3.TabIndex = 8;
+            this.tbPersoonY.Location = new System.Drawing.Point(65, 77);
+            this.tbPersoonY.Name = "tbPersoonY";
+            this.tbPersoonY.Size = new System.Drawing.Size(75, 20);
+            this.tbPersoonY.TabIndex = 8;
             // 
             // lbCoordinatenRobot
             // 
             this.lbCoordinatenRobot.AutoSize = true;
-            this.lbCoordinatenRobot.Location = new System.Drawing.Point(15, 105);
+            this.lbCoordinatenRobot.Location = new System.Drawing.Point(12, 105);
             this.lbCoordinatenRobot.Name = "lbCoordinatenRobot";
             this.lbCoordinatenRobot.Size = new System.Drawing.Size(90, 13);
             this.lbCoordinatenRobot.TabIndex = 7;
@@ -131,59 +183,60 @@
             // lbCoordinaatPersoon
             // 
             this.lbCoordinaatPersoon.AutoSize = true;
-            this.lbCoordinaatPersoon.Location = new System.Drawing.Point(15, 35);
+            this.lbCoordinaatPersoon.Location = new System.Drawing.Point(47, 35);
             this.lbCoordinaatPersoon.Name = "lbCoordinaatPersoon";
             this.lbCoordinaatPersoon.Size = new System.Drawing.Size(104, 13);
             this.lbCoordinaatPersoon.TabIndex = 6;
             this.lbCoordinaatPersoon.Text = "coordinaten persoon";
             // 
-            // textBox2
+            // tbPersoonX
             // 
-            this.textBox2.Location = new System.Drawing.Point(30, 51);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(75, 20);
-            this.textBox2.TabIndex = 1;
+            this.tbPersoonX.Location = new System.Drawing.Point(65, 51);
+            this.tbPersoonX.Name = "tbPersoonX";
+            this.tbPersoonX.Size = new System.Drawing.Size(75, 20);
+            this.tbPersoonX.TabIndex = 1;
             // 
             // btZendCoordinaten
             // 
-            this.btZendCoordinaten.Location = new System.Drawing.Point(15, 195);
+            this.btZendCoordinaten.Location = new System.Drawing.Point(50, 196);
             this.btZendCoordinaten.Name = "btZendCoordinaten";
-            this.btZendCoordinaten.Size = new System.Drawing.Size(110, 23);
+            this.btZendCoordinaten.Size = new System.Drawing.Size(101, 23);
             this.btZendCoordinaten.TabIndex = 5;
             this.btZendCoordinaten.Text = "zend coordinaten";
             this.btZendCoordinaten.UseVisualStyleBackColor = true;
             this.btZendCoordinaten.Click += new System.EventHandler(this.btZendCoordinaten_Click);
             // 
-            // textBox1
+            // tbRobot1Y
             // 
-            this.textBox1.Location = new System.Drawing.Point(30, 157);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(75, 20);
-            this.textBox1.TabIndex = 0;
+            this.tbRobot1Y.Location = new System.Drawing.Point(27, 157);
+            this.tbRobot1Y.Name = "tbRobot1Y";
+            this.tbRobot1Y.Size = new System.Drawing.Size(75, 20);
+            this.tbRobot1Y.TabIndex = 0;
             // 
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.groupBox2);
             this.groupBox3.Location = new System.Drawing.Point(12, 12);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(146, 374);
+            this.groupBox3.Size = new System.Drawing.Size(214, 374);
             this.groupBox3.TabIndex = 13;
             this.groupBox3.TabStop = false;
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.btStatus);
             this.groupBox2.Controls.Add(this.btKomTerug);
             this.groupBox2.Controls.Add(this.btVolgendeStap);
             this.groupBox2.Location = new System.Drawing.Point(0, 5);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(146, 93);
+            this.groupBox2.Size = new System.Drawing.Size(214, 110);
             this.groupBox2.TabIndex = 7;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "comands";
             // 
             // btKomTerug
             // 
-            this.btKomTerug.Location = new System.Drawing.Point(6, 19);
+            this.btKomTerug.Location = new System.Drawing.Point(50, 19);
             this.btKomTerug.Name = "btKomTerug";
             this.btKomTerug.Size = new System.Drawing.Size(99, 23);
             this.btKomTerug.TabIndex = 3;
@@ -193,7 +246,7 @@
             // 
             // btVolgendeStap
             // 
-            this.btVolgendeStap.Location = new System.Drawing.Point(6, 48);
+            this.btVolgendeStap.Location = new System.Drawing.Point(50, 48);
             this.btVolgendeStap.Name = "btVolgendeStap";
             this.btVolgendeStap.Size = new System.Drawing.Size(99, 23);
             this.btVolgendeStap.TabIndex = 4;
@@ -201,11 +254,21 @@
             this.btVolgendeStap.UseVisualStyleBackColor = true;
             this.btVolgendeStap.Click += new System.EventHandler(this.btVolgendeStap_Click);
             // 
+            // btStatus
+            // 
+            this.btStatus.Location = new System.Drawing.Point(50, 77);
+            this.btStatus.Name = "btStatus";
+            this.btStatus.Size = new System.Drawing.Size(99, 23);
+            this.btStatus.TabIndex = 5;
+            this.btStatus.Text = "geef status";
+            this.btStatus.UseVisualStyleBackColor = true;
+            this.btStatus.Click += new System.EventHandler(this.btStatus_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(690, 394);
+            this.ClientSize = new System.Drawing.Size(803, 394);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox3);
             this.Name = "Form1";
@@ -225,17 +288,23 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox tbRobot1X;
+        private System.Windows.Forms.TextBox tbPersoonY;
         private System.Windows.Forms.Label lbCoordinatenRobot;
         private System.Windows.Forms.Label lbCoordinaatPersoon;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tbPersoonX;
         private System.Windows.Forms.Button btZendCoordinaten;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbRobot1Y;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button btKomTerug;
         private System.Windows.Forms.Button btVolgendeStap;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbRobot2X;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbRobot2Y;
+        private System.Windows.Forms.Button btStatus;
 
     }
 }
--- laptop/Form1.cs
+++ laptop/Form1.cs
@@ -14,28 +14,48 @@ namespace laptop
         public Form1()
         {
             InitializeComponent();
+            
         }
 
         private void btKomTerug_Click(object sender, EventArgs e)
         {
             // zend signaal dat robot eigen weg terug vind
-            // stuur de bijbehorende byte (metode uit andere klas)
+
+            "0-3-48\n"; //robot1
+            "0-5-48\n"; //robot2
         }
 
         private void btVolgendeStap_Click(object sender, EventArgs e)
         {
-            // zend signaal dat hij de volgende stap mag uitvoeren 
-            // stuur de bijbehoorende byte (methode uit andere klas)
+            // zend signaal dat hij de volgende stap mag uitvoeren
+            "0-3-16\n"; //robot1
+            "0-5-16\n"; //robot2
+        }
+
+        private void btStatus_Click(object sender, EventArgs e)
+        {
+            // zend signaal dat hij de status geeft 
+            "0-3-0\n"; //robot1
+            "0-5-0\n"; //robot2
         }
 
         private void btZendCoordinaten_Click(object sender, EventArgs e)
         {
-            // zend de ingevulde coordianten naar de robot
-
-            // lees x,y van persoon
-            // stuur een byte
-            // lees x,y van wagen
-            // stuur een byte
+            //strings
+            // persoon naar bijde robots 
+            string xpersoon1 = "0-2-" + tbPersoonX.Text + "\n";
+            string xpersoon2 = "0-4-" + tbPersoonX.Text + "\n";
+            string ypersoon1 = "0-2-" + Convert.ToString(16 + Convert.ToInt32(tbPersoonY.Text)) + "\n";
+            string ypersoon2 = "0-4-" + Convert.ToString(16 + Convert.ToInt32(tbPersoonX.Text)) + "\n";
+            // robot1
+            string xwagen1 = "0-2-" + Convert.ToString(32 + Convert.ToInt32(tbRobot1X.Text)) + "\n";
+            string ywagen1 = "0-2-" + Convert.ToString(48 + Convert.ToInt32(tbRobot1Y.Text)) + "\n";
+            // robot 2
+            string xwagen2 = "0-4-" + Convert.ToString(32 + Convert.ToInt32(tbRobot2X.Text)) + "\n";
+            string ywagen2 = "0-4-" + Convert.ToString(48 + Convert.ToInt32(tbRobot2Y.Text)) + "\n";  
+   
+            // stuur de berichten
+            // send string (name) 
         }
 
         private void tekendoolhof()
