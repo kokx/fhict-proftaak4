08f7e17c89d0123662af3fc57ef72f2a60ceaefe
Pieter Kokx <pieter@kokx.nl>
2010-05-18 14:38:31 +0200

    Added header files for the HAL, IR and pathfinder.

---
+++ bot/control/hal.h
@@ -0,0 +1,24 @@
+/* 
+ * ****************************************************************************
+ * RP6 ROBOT SYSTEM - RP6 CONTROL M32 Examples
+ * ****************************************************************************
+ * HAL header file
+ * ****************************************************************************
+ */
+
+/*****************************************************************************/
+
+#ifndef _HAL_H__
+#define _HAL_H__
+
+    /* Includes */
+
+    /* Function prototypes */
+        void hal_hasWallLeft();
+        void hal_hasWallRight();
+        void hal_hasWallFront();
+        void hal_TurnLeft();
+        void hal_TurnRight();
+        void hal_MoveForward();
+
+#endif
