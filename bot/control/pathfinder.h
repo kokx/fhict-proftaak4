fc3c0bb450b5127861fb065a9b48669b396865b7
Pieter Kokx <pieter@kokx.nl>
2010-05-25 07:58:00 +0200

    Added defines, structures and function prototypes to the pathfinder header file.

--- bot/control/pathfinder.h
+++ bot/control/pathfinder.h
@@ -11,8 +11,26 @@
 #ifndef _PATHFINDER_H_
 #define _PATHFINDER_H_
 
+    /* Defines */
+    #define NORTH 0
+    #define WEST  1
+    #define SOUTH 2
+    #define EAST  3
+
+    /* Structures */
+    struct node {
+        uint8_t x;
+        uint8_t y;
+        struct node *north;
+        struct node *west;
+        struct node *south;
+        struct node *east;
+    };
+
     /* Includes */
 
     /* Function prototypes */
+    uint8_t pathfinder_NextStep(void);
+    uint8_t pathfinder_SetTarget(uint8_t x, uint8_t y);
 
 #endif
