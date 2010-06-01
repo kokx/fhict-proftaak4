6adc2bce641f220d414a2279b8b38d9e8ef5490b
Pieter Kokx <pieter@kokx.nl>
2010-06-01 10:23:49 +0200

    Added initial IR data structure.

--- bot/control/ir.h
+++ bot/control/ir.h
@@ -11,6 +11,12 @@
 #ifndef _IR_H_
 #define _IR_H_
 
+    struct ir_data {
+        uint8_t targetX;
+        uint8_t targetY;
+        uint8_t currentX;
+        uint8_t currentY;
+    };
     /* Includes */
 
     /* Function prototypes */
